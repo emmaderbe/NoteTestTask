89f9271021ccec1f55c82a9806fd02987f2ea34e
emmaderbe <emma-derbe@mail.ru>
2024-02-09 16:41:36 +0100

    Initial Commit

---
+++ NoteTestTask/NoteTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTestTaskApp.swift
+//  NoteTestTask
+//
+//  Created by Дербе Эмма on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NoteTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
